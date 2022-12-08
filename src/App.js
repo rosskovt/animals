ba19f79adc76eb2021987c9e49b9684012c08507
rosskovt <rostikk@gmail.com>
2022-12-08 22:30:48 +0200

    finished app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import './App.css';
+import { useState } from "react";
+import AnimalShow from "./AnimalShow";
+
+
+function getRandomAnimal() {
+    const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+
+    return animals[Math.floor(Math.random() * animals.length)];
+}
+
 function App() {
+    const [animals, setAnimals] = useState([]);
+    console.log(animals);
     const handleClick = () => {
-        console.log('Clicked')
+        setAnimals([...animals, getRandomAnimal()]);
     };
     return (
-        <div>
+        <div className='app'>
             <button onClick={handleClick}> Add Animal</button>
+            <div className='animal-list'>
+                {animals.map((animal, index) => {
+                    return <AnimalShow key={index} type={animal} />
+                })}
+            </div>
         </div>
 
     );
